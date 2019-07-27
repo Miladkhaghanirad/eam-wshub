3d69f1c56e0d33e530e57397758a7ae3a05f6cde
Lukasz Pater <Lukasz.Pater@cern.ch>
2019-07-27 22:34:13 +0200

    Deactive setUnmarshallingContextErrorsCounter

--- wshubejb/src/main/java/ch/cern/cmms/wshub/tools/InforClientProducer.java
+++ wshubejb/src/main/java/ch/cern/cmms/wshub/tools/InforClientProducer.java
@@ -3,6 +3,7 @@ package ch.cern.cmms.wshub.tools;
 import ch.cern.eam.wshub.core.client.InforClient;
 import ch.cern.cmms.wshub.handlers.SOAPHandlerResolver;
 import ch.cern.eam.wshub.core.tools.InforException;
+import com.sun.xml.bind.v2.runtime.unmarshaller.UnmarshallingContext;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -10,9 +11,8 @@ import javax.enterprise.concurrent.ManagedExecutorService;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import javax.sql.DataSource;
+import java.lang.reflect.Field;
+import java.util.logging.Level;
 
 @ApplicationScoped
 public class InforClientProducer {
@@ -21,22 +21,30 @@ public class InforClientProducer {
 	private InforClient inforClient;
 	@Inject
 	private ApplicationData applicationData;
-	//@Resource
-	//private DataSource datasource;
-	//@PersistenceUnit
-	//private EntityManagerFactory entityManagerFactory;
 	@Resource
 	private ManagedExecutorService executorService;
 	
 	@PostConstruct
 	public void init() throws InforException {
 		inforClient = new InforClient.Builder(applicationData.getInforWSURL(), applicationData.getTenant())
-				.withDefaultOrganizationCode("*")
 				.withSOAPHandlerResolver(new SOAPHandlerResolver())
-				//.withDataSource(datasource)
-				//.withEntityManagerFactory(entityManagerFactory)
 				.withExecutorService(executorService)
 				.build();
+
+		//TODO setUnmarshallingContextErrorsCounter();
 	}
+
+	/*
+	private void setUnmarshallingContextErrorsCounter() {
+		try {
+			inforClient.getTools().log(Level.INFO, "Setting UnmarshallingContext.errorsCounter to -1 to avoid unmarshaller errors about unknown properties.");
+			Field field = UnmarshallingContext.class.getDeclaredField("errorsCounter");
+			field.setAccessible(true);
+			field.setInt(null, -1);
+		} catch (Exception exception) {
+			inforClient.getTools().log(Level.SEVERE, "Couldn't set UnmarshallingContext.errorsCounter: " + exception.getMessage());
+		}
+	}
+	*/
 
 }
